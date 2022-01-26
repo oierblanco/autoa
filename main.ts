8a9feb43b04fb614bb65c27fba60f8d1522f5179
oierblanco <93262346+oierblanco@users.noreply.github.com>
2022-01-26 14:27:39 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,40 +1,57 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        for (let index = 0; index < 4; index++) {
-            basic.showLeds(`
-                . . . . .
-                # # . . .
-                # # . . .
-                # # . . .
-                . . . . .
-                `)
-        }
+        ezkerreko_intermitentea()
     }
     if (receivedNumber == 2) {
-        for (let index = 0; index < 4; index++) {
-            basic.showLeds(`
-                . . . . .
-                # # . . .
-                # # . . .
-                # # . . .
-                . . . . .
-                `)
-        }
+        eskuineko_intermitentea()
     }
     if (receivedNumber == 3) {
-    	
+        limpia_parabrisas()
     }
 })
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(1)
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(2)
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    radio.sendNumber(3)
-})
+function ezkerreko_intermitentea () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+}
+function limpia_parabrisas () {
+    for (let index = 0; index < 4; index++) {
+        pins.servoWritePin(AnalogPin.P0, 0)
+        basic.pause(100)
+        pins.servoWritePin(AnalogPin.P0, 180)
+        basic.pause(500)
+    }
+}
+function eskuineko_intermitentea () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . . .
+            `)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+}
 radio.setGroup(1)
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(1)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(2)
+    }
+    if (input.logoIsPressed()) {
+        radio.sendNumber(3)
+    }
 })
